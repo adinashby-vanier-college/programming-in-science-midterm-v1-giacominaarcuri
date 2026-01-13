69eefcba74c717eb5f2216977cef472eb348a144
giacominaarcuri <jackrie8116@gmail.com>
2026-01-13 10:59:19 -0500

    Add area_of_circle and triangle/pyramid functions
    Implemented area calculation for a circle and added functions for generating a hollow right triangle and an inverted pyramid.

--- Midterm.py
+++ Midterm.py
@@ -2,15 +2,48 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    area = math.pi * radius ** 2
+
+    return round(area, 2)
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    result = ""
+
+    if n < 4:
+        result += "The triangle height should be at least 4."
+    else:
+        for i in range(1, n + 1):
+            for j in range(i):
+                if j == 0 or i == n or j + 1 == i:
+                    result += "*"
+                else:
+                    result += " "
+
+            result += "\n"
+    
+    return result.rstrip()
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    result = ""
+
+    if n < 3:
+        result += "The pyramid height should be at least 3."
+    else:
+        for i in range(n):
+            for j in range(i):
+                result += " "
+
+            for k in range(n - i):
+                result += "*"
+        
+            for l in range(n - i - 1):
+                result += "*"
+
+            result += "\n"
+        
+    return result.rstrip()
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
